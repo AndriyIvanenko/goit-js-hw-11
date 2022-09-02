27db517c6d574c2a0ec501278ff50f17a42eda4d
Andriy Ivanenko <andriy.ivanenko@gmail.com>
2022-09-02 23:42:00 +0300

    axios added

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,8 +1,11 @@
+import axios from 'axios';
+
 export async function fetchImages(request, page, imagesPerPage) {
-  const response = await fetch(
+  const response = await axios.get(
     `https://pixabay.com/api/?key=29601825-65f79e377599d679ceb963779&q=${request}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${imagesPerPage}`
   );
-  const images = await response.json();
+  // console.log(response);
+  const images = response.data;
   if (images.totalHits === 0) {
     throw new Error();
   }
